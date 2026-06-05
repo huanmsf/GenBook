2c54a4050b792f4da0a796bde0ed3a1c395bfe07
huanmsf <huan.xiong@yahoo.com>
2026-06-05 08:59:24 +0800

    页码

---
+++ _tw.py
@@ -0,0 +1,3 @@
+import pathlib
+f=pathlib.Path(chr(109)+chr(111)+chr(100)+chr(117)+chr(108)+chr(101)+chr(115)+chr(47)+chr(116)+chr(121)+chr(112)+chr(115)+chr(116)+chr(95)+chr(119)+chr(114)+chr(105)+chr(116)+chr(101)+chr(114)+chr(46)+chr(112)+chr(121)).read_bytes().decode(chr(117)+chr(116)+chr(102)+chr(45)+chr(56))
+print(chr(100)+chr(111)+chr(110)+chr(101))
